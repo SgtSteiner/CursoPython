2fd345cd74edabd13b77e05c3004643cb2311ec1
Sgt Steiner <futitotal@gmail.com>
2017-06-18 17:23:24 +0200

    KochCurve Dibuja una curva de Koch

---
+++ src/KochCurve.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 18 jun. 2017
+
+@author: Steiner
+'''
+
+import turtle
+
+def dibujaKoch(t, n):
+    """ Dibuja una curva de Koch de longitud "n". t es una turtle"""
+    if n < 10:
+        t.fd(n)
+        return
+    m = n / 3
+    dibujaKoch(t, m)
+    t.lt(60)
+    dibujaKoch(t, m)
+    t.rt(120)
+    dibujaKoch(t, m)
+    t.lt(60)
+    dibujaKoch(t, m)
+
+def coponieve(t, n):
+    """ Dibuja un copo de nieve (un triángulo con
+    una curva de Koch en su interior)"""
+    for i in range(3):
+        dibujaKoch(t, n)
+        t.rt(120)
+    
+bob = turtle.Turtle()
+
+coponieve(bob, 250)
+turtle.mainloop()
