f5734bed2f8d6011dd4e37ea52c21e2799e0676f
Sgt Steiner <futitotal@gmail.com>
2017-07-02 19:12:43 +0200

    act

---
+++ src/GoodKangaroo.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2 jul. 2017
+
+@author: Steiner
+'''
+class Kangaroo:
+    
+    def __init__(self, name, pouch_content=None):
+        self.name = name
+        if pouch_content == None:
+            pouch_content = []
+        self.pouch_content = pouch_content
+    
+    def __str__(self):
+        t = ["Contenido de %s: " % self.name ]
+        for obj in self.pouch_content:
+            s = "   " + object.__str__(obj)
+            t.append(s)
+        return "\n".join(t)
+            
+    def put_in_pouch(self, item):
+        self.pouch_content.append(item)
+        
+
+kanga = Kangaroo("kanga")
+roo = Kangaroo("roo")
+
+a = [23, 3]
+kanga.put_in_pouch("Llaves")
+kanga.put_in_pouch(25.6)
+kanga.put_in_pouch(divmod(25,4))
+kanga.put_in_pouch(a)
+kanga.put_in_pouch(roo)
+
+print(kanga)
+print(roo)
